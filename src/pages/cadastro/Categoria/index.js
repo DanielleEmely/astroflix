5c65df5e38009e09f0f4a61e00d51c16bd28b4e9
DanielleEmely <danielleemily92014@gmail.com>
2020-07-29 00:56:41 -0300

    Mudanças da aula 2 - Parte 2

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -7,6 +7,18 @@ function CadastroCategoria(){
       <PageDefault>
         <h1>Cadastro de Categoria</h1>
         
+        <form>
+          <label>
+            Nome da Categoria:
+            <input
+              type="text"
+            />
+          </label>
+          <button>
+            Cadastrar
+          </button>
+        </form>
+
         <Link to="/">
             Ir para home
         </Link>
